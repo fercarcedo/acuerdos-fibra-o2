2f2acf8cbd6a393fab06faef3f5a6a2c625f1558
Fernando García Álvarez <fercarcedo@gmail.com>
2023-12-01 23:45:47 +0100

    feat: parse code and display fiber agreement based on it, replace text if already present

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,29 @@
-function changeBackgroundColor() {
+function getFiberTypeText(coverage_area_id) {
+  switch (coverage_area_id) {
+    case '501000':
+      return 'Fibra directa Movistar';
+    case '501005':
+      return 'A través de Adamo';
+    case '501006':
+      return 'A través de Orange';
+  }
+  return 'Acuerdo de fibra desconocido';
+}
+
+function displayFiberType(fiber_type_text) {
   const coverageResultContainer = document.getElementById('coverAlert');
   const fiberTypeParagraph = document.createElement('p');
+  fiberTypeParagraph.classList.add('fiber-type-text');
   fiberTypeParagraph.style.fontSize = '16px';
   fiberTypeParagraph.style.color = '#000';
   fiberTypeParagraph.style.marginBottom = 0;
-  fiberTypeParagraph.innerHTML = 'A través de Adamo';
-  coverageResultContainer.appendChild(fiberTypeParagraph);
+  fiberTypeParagraph.innerHTML = fiber_type_text;
+  const alreadyAddedElement = coverageResultContainer.querySelector('p.fiber-type-text');
+  if (alreadyAddedElement) {
+    coverageResultContainer.replaceChild(fiberTypeParagraph, alreadyAddedElement);
+  } else {
+    coverageResultContainer.appendChild(fiberTypeParagraph);
+  }
 }
 
 chrome.webRequest.onCompleted.addListener(async (details) => {
@@ -17,7 +35,8 @@ chrome.webRequest.onCompleted.addListener(async (details) => {
       console.log(result.coverage_area_id);
       await chrome.scripting.executeScript({
         target: { tabId: details.tabId, frameIds: [details.frameId] },
-        func: changeBackgroundColor,
+        func: displayFiberType,
+        args: [getFiberTypeText(result.coverage_area_id)]
       });
     }
   }
